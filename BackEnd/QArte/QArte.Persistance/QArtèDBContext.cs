7265be5def7fc540c32bcce9275cb863524c6801
Martin Kolev <martin.kolev@blankfactor.com>
2024-01-10 11:54:20 +0200

    Adding DataBase Context file and updating the connection to the data base

---
+++ BackEnd/QArte/QArte.Persistance/QArtèDBContext.cs
@@ -0,0 +1,36 @@
+﻿using QArte.Persistance.PersistanceModels;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QArte.Persistance
+{
+    public partial class QArtèDBContext : DbContext
+    {
+        public QArtèDBContext()
+        {
+        }
+
+        public QArtèDBContext(DbContextOptions<QArtèDBContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Admin> Admins { get; set; }
+        public virtual DbSet<BankAccount> BankAccounts { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<BanTable> BanTables { get; set; }
+        public virtual DbSet<Fee> Fees { get; set; }
+        public virtual DbSet<Gallery> Galleries { get; set; }
+        public virtual DbSet<Invoice> Invoices { get; set; }
+        public virtual DbSet<Page> Pages { get; set; }
+        public virtual DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public virtual DbSet<Picture> Pictures { get; set; }
+        public virtual DbSet<SettlementCycle> SettlementCycles { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+
+    }
+}
+
+
