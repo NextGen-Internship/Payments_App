97d8981d3319ef66f10228c31dd58e5cb7f0b382
Martin Kolev <martin.kolev@blankfactor.com>
2024-01-17 15:18:15 +0200

    Role Service

--- BackEnd/QArte/QArte.Services/Services/RoleService.cs
+++ BackEnd/QArte/QArte.Services/Services/RoleService.cs
@@ -11,33 +11,87 @@ namespace QArte.Services.Services
 {
 	public class RoleService : IRoleService
 	{
-		public RoleService()
+        private readonly QArteDBContext _qArteDBContext;
+
+		public RoleService(QArteDBContext qArteDBContext)
 		{
+            _qArteDBContext = qArteDBContext;
 		}
 
-        public Task<RoleDTO> DeleteAsync(int id)
+
+        public async Task<RoleDTO> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var role = await this._qArteDBContext.Roles
+                .FirstOrDefaultAsync(x => x.ID == id)
+                ?? throw new ApplicationException("Not found");
+
+            this._qArteDBContext.Roles.Remove(role);
+            await _qArteDBContext.SaveChangesAsync();
+
+            return role.GetDTO();
         }
 
-        public Task<IEnumerable<RoleDTO>> GetAsync()
+        public async Task<IEnumerable<RoleDTO>> GetAsync()
         {
-            throw new NotImplementedException();
+            return await this._qArteDBContext.Roles
+                .Select(x => new RoleDTO
+                {
+                    ID = x.ID,
+                    ERole = x.ERole,
+                    Users = x.Users
+                    .Select(y => new UserDTO
+                    {
+                        ID = y.ID,
+
+                    }).ToList()
+                }).ToListAsync();
         }
 
-        public Task<RoleDTO> GetRoleByID(int id)
+        public async Task<RoleDTO> GetRoleByID(int id)
         {
-            throw new NotImplementedException();
+            var role = await _qArteDBContext.Roles
+              .Include(x=>x.Users)
+              .FirstOrDefaultAsync(x => x.ID == id)
+              ?? throw new ApplicationException("Not found");
+            return role.GetDTO();
         }
 
-        public Task<RoleDTO> PostAsync(RoleDTO obj)
+        public async Task<RoleDTO> PostAsync(RoleDTO obj)
         {
-            throw new NotImplementedException();
+            RoleDTO result = null;
+
+            var deletedRole = await _qArteDBContext.Roles
+                                            .Include(x => x.Users)
+                                            .IgnoreQueryFilters()
+                                            .FirstOrDefaultAsync(x => x.ID== obj.ID);
+            var newRole = obj.GetEnity();
+            if (deletedRole == null)
+            {
+                await this._qArteDBContext.Roles.AddAsync(newRole);
+                await _qArteDBContext.SaveChangesAsync();
+                result = newRole.GetDTO();
+            }
+            else
+            {
+                result = deletedRole.GetDTO();
+
+            }
+
+            return result;
         }
 
-        public Task<RoleDTO> UpdateAsync(int id, RoleDTO obj)
+        public async Task<RoleDTO> UpdateAsync(int id, RoleDTO obj)
         {
-            throw new NotImplementedException();
+            var Role = await this._qArteDBContext.Roles
+                .Include(x => x.Users)
+                .FirstOrDefaultAsync(x => x.ID == id)
+                ?? throw new ApplicationException("Not found");
+
+            Role.ID = obj.ID;
+            Role.ERole = obj.ERole;
+            await _qArteDBContext.SaveChangesAsync();
+
+            return Role.GetDTO();
         }
     }
 }
