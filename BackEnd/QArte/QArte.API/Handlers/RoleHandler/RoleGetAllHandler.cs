989ca1d4e42083d1ae392b4707658605eefbce08
Martin Kolev <martin.kolev@blankfactor.com>
2024-01-17 15:47:31 +0200

    Role Controller

---
+++ BackEnd/QArte/QArte.API/Handlers/RoleHandler/RoleGetAllHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using QArte.API.Queries.RoleQuery;
+using QArte.Services.DTOs;
+using QArte.Services.ServiceInterfaces;
+using MediatR;
+
+namespace QArte.API.Handlers.RoleHandler
+{
+	public class RoleGetAllHandler : IRequestHandler<RoleGetAllQuery, List<RoleDTO>>
+	{
+        private readonly IRoleService _roleService;
+
+        public RoleGetAllHandler(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task<List<RoleDTO>> Handle(RoleGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _roleService.GetAsync();
+            return (List<RoleDTO>)order;
+        }
+    }
+}
+
---
+++ BackEnd/QArte/QArte.API/Handlers/RoleHandler/RolePatchHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using QArte.API.Commands.RoleCommands;
+using QArte.Services.DTOs;
+using QArte.Services.ServiceInterfaces;
+using MediatR;
+
+namespace QArte.API.Handlers.RoleHandler
+{
+	public class RolePatchHandler : IRequestHandler<RolePatchCommand,RoleDTO>
+	{
+        private readonly IRoleService _roleService;
+
+        public RolePatchHandler(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task<RoleDTO> Handle(RolePatchCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _roleService.UpdateAsync(request.id,request.roleDTO);
+            return order;
+        }
+    }
+}
+
