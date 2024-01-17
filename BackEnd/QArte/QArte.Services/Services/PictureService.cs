7d2cfb2adf0c43a5cca8a870447a8aa5a1c3768a
Martin Kolev <martin.kolev@blankfactor.com>
2024-01-17 13:55:25 +0200

    no message

--- BackEnd/QArte/QArte.Services/Services/PictureService.cs
+++ BackEnd/QArte/QArte.Services/Services/PictureService.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using QArte.Services.DTOs;
+using QArte.Services.DTOMappers;
+using QArte.Services.ServiceInterfaces;
+using Microsoft.EntityFrameworkCore;
+using QArte.Persistance.Enums;
+using QArte.Persistance;
+using Microsoft.VisualBasic;
+using QArte.Persistance.PersistanceModels;
+
 namespace QArte.Services.Services
 {
-	public class PictureService
+	public class PictureService : IPictureService
 	{
 		public PictureService()
 		{
 		}
-	}
+
+        public Task<PictureDTO> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PictureDTO>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PictureDTO> GetPictureByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PictureDTO> GetPicturesByGalleryID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PictureDTO> PostAsync(PictureDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PictureDTO> UpdateAsync(int id, PictureDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
 
