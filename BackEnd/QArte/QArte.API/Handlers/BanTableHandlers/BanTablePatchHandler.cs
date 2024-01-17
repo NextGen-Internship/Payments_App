3b12e78a88471854353d553e8de77b50a499ec9e
Martin Kolev <martin.kolev@blankfactor.com>
2024-01-17 13:30:57 +0200

    BanTable Controller

---
+++ BackEnd/QArte/QArte.API/Handlers/BanTableHandlers/BanTablePatchHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using QArte.API.Commands.BanTableCommands;
+using QArte.Services.DTOs;
+using QArte.Services.ServiceInterfaces;
+using MediatR;
+
+namespace QArte.API.Handlers.BanTableHandlers
+{
+	public class BanTablePatchHandler : IRequestHandler<BanTablePatchCommand,BanTableDTO>
+	{
+        private readonly IBanTableService _banTableService;
+		public BanTablePatchHandler(IBanTableService banTableService)
+		{
+            _banTableService = banTableService;
+		}
+
+        public async Task<BanTableDTO> Handle(BanTablePatchCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _banTableService.UpdateAsync(request.id, request.BankAccountDTO);
+            return order;
+        }
+    }
+}
+
