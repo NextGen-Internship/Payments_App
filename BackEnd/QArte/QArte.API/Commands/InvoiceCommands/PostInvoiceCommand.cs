f40fc05d4af420829403ce49ada26a78bcf6ec65
emakyuchukova <ema.kyuchukova@blankfactor.com>
2024-01-22 11:30:45 +0200

    try

---
+++ BackEnd/QArte/QArte.API/Commands/InvoiceCommands/PostInvoiceCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QArte.API.Queries;
+using QArte.Services.DTOs;
+using QArte.Services.ServiceInterfaces;
+using QArte.Services.DTOMappers;
+using MediatR;
+
+namespace QArte.API.Commands.InvoiceCommands
+{
+    public class PostInvoiceCommand : IRequest<InvoiceDTO>
+    {
+        public InvoiceDTO InvoiceDTO;
+
+        public PostInvoiceCommand(InvoiceDTO invoiceDTO)
+        {
+            InvoiceDTO = invoiceDTO;
+        }
+    }
+}
---
+++ BackEnd/QArte/QArte.API/Queries/SettlementCycleQueries/GetSettlementCycleBeforeDateQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QArte.API.Queries;
+using QArte.Services.DTOs;
+using QArte.Services.ServiceInterfaces;
+using QArte.Services.DTOMappers;
+using MediatR;
+
+namespace QArte.API.Queries.SettlementCycleQueries
+{
+	public class GetSettlementCycleBeforeDateQuery : IRequest<List<SettlementCycleDTO>>
+    {
+		public DateTime date;
+        public GetSettlementCycleBeforeDateQuery(DateTime date)
+        {
+            this.date = date;
+        }
+    }
+}
+
