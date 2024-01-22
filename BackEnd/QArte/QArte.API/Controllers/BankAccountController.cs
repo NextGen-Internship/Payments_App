b283d545253d61b178cb7e9abda50cb4084bb754
emakyuchukova <ema.kyuchukova@blankfactor.com>
2024-01-22 11:35:25 +0200

    no message

--- BackEnd/QArte/QArte.API/Controllers/BankAccountController.cs
+++ BackEnd/QArte/QArte.API/Controllers/BankAccountController.cs
@@ -66,6 +66,8 @@ namespace QArte.API.Controllers
         }
 
 
+
+
 		[HttpPost]
 		public async Task<ActionResult<BankAccountDTO>> PostBankAccount([FromBody] BankAccountDTO obj)
 		{
--- BackEnd/QArte/QArte.Services/Services/PaymentMethodService.cs
+++ BackEnd/QArte/QArte.Services/Services/PaymentMethodService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using QArte.Services.DTOs;
+﻿using QArte.Services.DTOs;
 using QArte.Services.DTOMappers;
 using QArte.Services.ServiceInterfaces;
 using Microsoft.EntityFrameworkCore;
@@ -12,134 +11,44 @@ namespace QArte.Services.Services
 {
 	public class PaymentMethodService : IPaymentMethodsService
 	{
-        private readonly QArteDBContext _qArteDBContext;
+		public PaymentMethodService()
+		{
+		}
 
-        public PaymentMethodService(QArteDBContext _qArteDBContext)
+        public Task<PaymentMethodDTO> DeleteAsync(int id)
         {
-            this._qArteDBContext = _qArteDBContext;
+            throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<PaymentMethodDTO>> GetAsync()
+        public Task<IEnumerable<PaymentMethodDTO>> GetAsync()
         {
-            return await _qArteDBContext.PaymentMethods
-                            .Select(x => new PaymentMethodDTO
-                            {
-                                ID = x.ID,
-                                paymentName = x.PaymentMethods,
-                                BankAccounts = x.BankAccounts.Select(y => new BankAccountDTO
-                                {
-                                    ID = y.ID,
-                                    IBAN = y.IBAN,
-                                    BeneficiaryName = y.BeneficiaryName,
-                                    StripeInfo = y.StripeInfo,
-                                    PaymentMethodID = y.PaymentMethodID,
-                                }).ToList()
-                            }).ToListAsync();
+            throw new NotImplementedException();
         }
 
-        public async Task<PaymentMethodDTO> GetPaymentMethodByID(int id)
+        public Task<PaymentMethodDTO> GetPaymentMethodByID(int id)
         {
-            var result = await _qArteDBContext.PaymentMethods
-                            .FirstOrDefaultAsync(x=> x.ID == id)
-                            ?? throw new ApplicationException("Not found");
-            return result.GetDTO();
+            throw new NotImplementedException();
         }
 
-      
-
-        public async Task<PaymentMethodDTO> GetPaymentMethodByUserID(int id)
+        public Task<PaymentMethodDTO> GetPaymentMethodByPaymentType(EPaymentMethods ePaymentMethod)
         {
-            var result = await _qArteDBContext.PaymentMethods
-                            .FirstOrDefaultAsync(x => x.ID == id)
-                            ?? throw new ApplicationException("Not found");
-            return result.GetDTO();
+            throw new NotImplementedException();
         }
 
-
-        public async Task<bool> PaymentMethodExists(int id)
+        public Task<PaymentMethodDTO> GetPaymentMethodByUserID(int id)
         {
-            return await _qArteDBContext.PaymentMethods.AnyAsync(x => x.ID == id);
+            throw new NotImplementedException();
         }
 
-
-        public async Task<PaymentMethodDTO> PostAsync(PaymentMethodDTO obj)
+        public Task<PaymentMethodDTO> PostAsync(PaymentMethodDTO obj)
         {
-            //var newPaymentMethod = obj.GetEntity();
-
-            //await _qArteDBContext.PaymentMethods.AddAsync(newPaymentMethod);
-            //await _qArteDBContext.SaveChangesAsync();
-
-            //return newPaymentMethod.GetDTO();
-
-            _ = await PaymentMethodExists(obj.ID)
-                == true ? throw new ApplicationException("Not found") : 0;
-
-            PaymentMethodDTO result = null;
-
-            var deletedPaymentMethod = await _qArteDBContext.PaymentMethods
-                                            .IgnoreQueryFilters()
-                                            .FirstOrDefaultAsync(x => x.ID == obj.ID);
-            var newPaymentMethod = obj.GetEnity();
-            if (deletedPaymentMethod == null)
-            {
-                await this._qArteDBContext.PaymentMethods.AddAsync(newPaymentMethod);
-                await _qArteDBContext.SaveChangesAsync();
-                result = deletedPaymentMethod.GetDTO();
-            }
-            else
-            {
-                result = deletedPaymentMethod.GetDTO();
-
-            }
-
-            return result;
+            throw new NotImplementedException();
         }
 
-        public async Task<PaymentMethodDTO> UpdateAsync(int id, PaymentMethodDTO obj)
+        public Task<PaymentMethodDTO> UpdateAsync(int id, PaymentMethodDTO obj)
         {
-            //var paymentMethod = await _qArteDBContext.PaymentMethods
-            //            .Include(x => x.PaymentMethods)
-            //            .FirstOrDefaultAsync(x => x.ID == id)
-            //            ?? throw new ApplicationException("Not found");
-
-            //paymentMethod.ID = obj.ID;
-            //await _qArteDBContext.SaveChangesAsync();
-
-            //return paymentMethod.GetDTO();
-
-            _ = await PaymentMethodExists(obj.ID)
-                == true ? throw new ApplicationException("Not found") : 0;
-
-            var paymentMethod = await this._qArteDBContext.PaymentMethods
-                        .Include(x => x.BankAccounts)
-                    .FirstOrDefaultAsync(x => x.ID == id)
-                    ?? throw new ApplicationException("Not found");
-
-
-            paymentMethod.ID = obj.ID;
-            paymentMethod.PaymentMethods = obj.paymentName;
-            
-            await _qArteDBContext.SaveChangesAsync();
-            return paymentMethod.GetDTO();
+            throw new NotImplementedException();
         }
-
-
-        public async Task<PaymentMethodDTO> DeleteAsync(int id)
-        {
-            var paymentMethod = await _qArteDBContext.PaymentMethods
-                        .Include(x => x.BankAccounts)
-                        .Include(x => x.PaymentMethods)
-                        .FirstOrDefaultAsync(x => x.ID == id)
-                        ?? throw new ApplicationException("Not found");
-
-            this._qArteDBContext.PaymentMethods.Remove(paymentMethod);
-            await _qArteDBContext.SaveChangesAsync();
-
-            return paymentMethod.GetDTO();
-
-
-        }
-
     }
 }
 
