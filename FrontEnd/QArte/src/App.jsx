4bc5b9fe4cb0177641c653509b9a9bee706b2b2a
emakyuchukova <ema.kyuchukova@blankfactor.com>
2024-01-30 10:18:05 +0200

    t push --all --force git push --tags --forceFrontEnd_new

--- FrontEnd/QArte/src/App.jsx
+++ FrontEnd/QArte/src/App.jsx
@@ -1,44 +1,68 @@
 import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import LoginSignUp from './Components/Access/LoginSignUp';
-// import LoginSignUp from './Components/Access/LoginSignUp';
+import LoginPage from './Components/Access/LoginPage';
+import SignupPage from './Components/Access/SignUpPage';
 
-function App() {
-    return (
-        <GoogleOAuthProvider clientId="">
-            <Router>
-                <Navbar />
-                <Routes>
-                    {/* <Route path="/home-page" element={<Home/>} />
-                    <Route path="/about-page" element={<About/>} />
-                    <Route path="/blog-page" element={<Blog/>} />
-                    <Route path="/explore-page" element={<Explore/>} /> */}
-                    <Route path="/login-signup" element={<LoginSignUp />} />
-                </Routes>
-            </Router>
-        </GoogleOAuthProvider>
-    );
-}
+const App = () => {
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+
+  const errorMessage = (error) => {
+    console.log(error);
+  };
+
+  return (
+    <GoogleOAuthProvider clientId="">
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Placeholder for other routes */}
+          {/* <Route path="/home" element={<div>Home Page</div>} />
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/contact" element={<div>Contact Page</div>} /> */}
+
+          {/* Route for the login page */}
+          <Route
+            path="/login-page"
+            element={<LoginPage responseMessage={responseMessage} errorMessage={errorMessage} />}
+          />
+
+          {/* Route for the signup page */}
+          <Route path="/signup-page" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
+  );
+};
 
 export default App;
 
+// import React from 'react';
+// import { GoogleLogin } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './Components/Navbar';
+// import LoginSignUp from './Components/Access/LoginSignUp';
+// // import LoginSignUp from './Components/Access/LoginSignUp';
+
 // function App() {
-//     const responseMessage = (response) => {
-//         console.log(response);
-//     };
-//     const errorMessage = (error) => {
-//         console.log(error);
-//     };
 //     return (
-//         <div>
-//             <h2>React Google Login</h2>
-//             <br />
-//             <br />
-//             <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-//         </div>
-//     )
+//         <GoogleOAuthProvider clientId="">
+//             <Router>
+//                 <Navbar />
+//                 <Routes>
+//                     {/* <Route path="/home-page" element={<Home/>} />
+//                     <Route path="/about-page" element={<About/>} />
+//                     <Route path="/blog-page" element={<Blog/>} />
+//                     <Route path="/explore-page" element={<Explore/>} /> */}
+//                     <Route path="/login-signup" element={<LoginSignUp />} />
+//                 </Routes>
+//             </Router>
+//         </GoogleOAuthProvider>
+//     );
 // }
+
 // export default App;
--- FrontEnd/QArte/src/Components/Navbar.jsx
+++ FrontEnd/QArte/src/Components/Navbar.jsx
@@ -1,37 +1,86 @@
 import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
-import './Navbar.css'
-
+import { Link } from "react-router-dom";
+import './Navbar.css';
 
 class Navbar extends Component {
-    state = {clicked:false};
-    handleClick = () => {
-        this.setState({clicked:!this.state.clicked});
-    }
-    render() {
-        return (
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">QArte</h1>
+  state = { clicked: false };
 
-                <div className="menu-icons" onClick={this.handleClick}>
-                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
-                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-                {MenuItems.map((item, index) => (
-                        <li key={index}>
-                            <a className={item.cName} href={item.url}>
-                                <i className={`nav-links ${item.icon}`}> </i>
-                                    {item.title}
-                            </a>
-                        </li>
-                        
-                ))}
-                <a href="/login-signup"><button>SignIn/SignUp</button></a>
-                </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">QArte</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => (
+            <li key={index}>
+              <Link className={item.cName} to={item.url}>
+                <i className={`nav-links ${item.icon}`}></i>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+          
+          <li>
+            <Link to="/login">
+              <button className="nav-links-mobile">Login</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup">
+              <button className="nav-links-mobile">Sign Up</button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
+
+// import React, { Component } from "react";
+// import { MenuItems } from "./MenuItems";
+// import './Navbar.css'
+
+
+// class Navbar extends Component {
+//     state = {clicked:false};
+//     handleClick = () => {
+//         this.setState({clicked:!this.state.clicked});
+//     }
+//     render() {
+//         return (
+//             <nav className="NavbarItems">
+//                 <h1 className="navbar-logo">QArte</h1>
+
+//                 <div className="menu-icons" onClick={this.handleClick}>
+//                      <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+//                 </div>
+
+//                 <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+//                 {MenuItems.map((item, index) => (
+//                         <li key={index}>
+//                             <a className={item.cName} href={item.url}>
+//                                 <i className={`nav-links ${item.icon}`}> </i>
+//                                     {item.title}
+//                             </a>
+//                         </li>
+                        
+//                 ))}
+//                 <a href="/login-signup"><button>SignIn/SignUp</button></a>
+//                 </ul>
+//             </nav>
+//         );
+//     }
+// }
+
+// export default Navbar;
