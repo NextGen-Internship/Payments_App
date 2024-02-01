ff1a89fe740387e13a2ec2877c17fa66d4f14308
Martin Kolev <martin.kolev@blankfactor.com>
2024-02-01 15:20:11 +0200

    Ema merge

--- FrontEnd/QArte/src/Components/Navbar.jsx
+++ FrontEnd/QArte/src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { MenuItems } from "./MenuItems";
+import { MenuItems } from "./MenuItems";import { Link } from "react-router-dom";
+import './Navbar.css';
 import './Navbar.css'
 import {NavLink } from 'react-router-dom';
 
@@ -13,10 +14,6 @@ class Navbar extends Component {
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">QArté</h1>
 
-                <div className="menu-icons" onClick={this.handleClick}>
-                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
-
                 <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                 {MenuItems.map((item, index) => (
                         <li key={index}>
@@ -27,11 +24,46 @@ class Navbar extends Component {
                         </li>
                         
                 ))}
-                <NavLink to="/login-signup"><button>SignIn/SignUp</button></NavLink>
+                {/* <NavLink to="/login-signup"><button>SignIn/SignUp</button></NavLink> */}
                 </ul>
             </nav>
         );
     }
-}
 
+  // render() {
+  //   return (
+  //     <nav className="NavbarItems">
+  //       <h1 className="navbar-logo">QArte</h1>
+
+  //       <div className="menu-icons" onClick={this.handleClick}>
+  //         <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+  //       </div>
+
+  //       <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+  //         {MenuItems.map((item, index) => (
+  //           <li key={index}>
+  //             <Link className={item.cName} to={item.url}>
+  //               <i className={`nav-links ${item.icon}`}></i>
+  //               {item.title}
+  //             </Link>
+  //           </li>
+  //         ))}
+          
+  //         <li>
+  //           <Link to="/login">
+  //             <button className="nav-links-mobile">Login</button>
+  //           </Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/signup">
+  //             <button className="nav-links-mobile">Sign Up</button>
+  //           </Link>
+  //         </li>
+  //       </ul>
+  //     </nav>
+  //   );
+  // }
+
+}
 export default Navbar;
+
