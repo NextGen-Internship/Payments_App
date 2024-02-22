7a1da4b0a520822e888b575cfec7edda61175bd7
Martin Kolev <martin.kolev@blankfactor.com>
2024-02-22 12:48:17 +0200

    Only the card is now a link, not the whole row

--- FrontEnd/QArte/src/Components/ExplorePage/UserCard/UserCard.tsx
+++ FrontEnd/QArte/src/Components/ExplorePage/UserCard/UserCard.tsx
@@ -4,70 +4,74 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
 import { Box } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 const OutlinedCard = ({ user }: any) => {
   return (
     <Box
-      sx={{
-        margin: "2% 20%",
-        "@media (max-width:600px)": {
-          margin: "2% 5%",
-        },
-      }}
-    >
-      <Card
-        variant="outlined"
         sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          minHeight: "15vh",
+          margin: "2% 20%",
+          "@media (max-width:600px)": {
+            margin: "2% 5%",
+          },
         }}
       >
-        <CardMedia
-          component="img"
+      <NavLink  to={`${user.id}`} style={{ textDecoration: "none" }}>
+        <Card
+          variant="outlined"
           sx={{
-            width: 180,
-            height: 180,
-            borderRadius: "50%",
-            marginRight: "20px",
-            objectFit: "cover",
-            "@media (max-width:600px)": {
-              width: 140,
-              height: 140,
-            },
-          }}
-          image={user.pictureURL}
-          alt="User Picture"
-        />
-        <CardContent
-          style={{
             display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            flexGrow: 1,
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            minHeight: "15vh",
           }}
         >
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{ mt: { xs: "10px", sm: "0" }, textAlign: "left" }}
+          <CardMedia
+            component="img"
+            sx={{
+              width: 180,
+              height: 180,
+              borderRadius: "50%",
+              marginRight: "20px",
+              objectFit: "cover",
+              "@media (max-width:600px)": {
+                width: 140,
+                height: 140,
+              },
+            }}
+            image={user.pictureURL}
+            alt="User Picture"
+          />
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              flexGrow: 1,
+            }}
           >
-            {user.username}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            gutterBottom
-            sx={{ textAlign: "left" }}
-          >
-            {`${user.firstName} ${user.lastName}`}
-          </Typography>
-        </CardContent>
-      </Card>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ mt: { xs: "10px", sm: "0" }, textAlign: "left" }}
+            >
+              {user.username}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              gutterBottom
+              sx={{ textAlign: "left" }}
+            >
+              {`${user.firstName} ${user.lastName}`}
+            </Typography>
+          </CardContent>
+        </Card>
+      </NavLink>
     </Box>
   );
+  
 };
 
 export default OutlinedCard;
